197af85bbf40de3573ac8348d87089d29baf36df
Carlos Jose Martinez Sanchez <44618871+W0lfMeN@users.noreply.github.com>
2022-08-26 11:40:41 +0200

    V2.0 Now you can give a single .mkv file to convert

--- mkvToMp4Converter.py
+++ mkvToMp4Converter.py
@@ -1,5 +1,6 @@
 import os
 import ffmpeg # pip install ffmpeg-python
+import sys
 
 start_dir = os.getcwd() # Get the file path
 
@@ -10,12 +11,27 @@ def convert_to_mp4(mkv_file):
     ffmpeg.input(mkv_file).output(out_name).run()
     print("Finished converting {}".format(mkv_file))
 
-# for-loop that iterate all the files in the actual path
-for path, folder, files in os.walk(start_dir):
-    for file in files:
-        # If the loop find a .mkv file, call the previous function to convert it
-        if file.endswith('.mkv'):
-            print("Found file: %s" % file)
-            convert_to_mp4(os.path.join(start_dir, file))
-        else:
-            pass
+
+def convert_folder_to_mp4():
+    # for-loop that iterate all the files in the actual path
+    for path, folder, files in os.walk(start_dir):
+        for file in files:
+            # If the loop find a .mkv file, call the previous function to convert it
+            if file.endswith('.mkv'):
+                print("Found file: %s" % file)
+                convert_to_mp4(os.path.join(start_dir, file))
+            else:
+                pass
+
+
+if __name__ == "__main__":
+    if(len(sys.argv)==1): # checks if the user has opened the script with a file, in this case the script will only convert that file. 
+                          # Otherwise will convert all the .mvk files in the actual folder
+        print("No file has been passed, all the .mvk files in actual folder will be convert. Press any key to continue")
+        input()
+        convert_folder_to_mp4()
+    else:
+        print("a file has been passed: "+sys.argv[1])
+        print("press any key to continue")
+        input()
+        convert_to_mp4(sys.argv[1])
